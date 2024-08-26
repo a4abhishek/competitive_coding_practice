99743e845f218d6e7bd0f17df1c7ec38da340d37
Abhishek Kashyap <avskksyp@gmail.com>
2024-08-26 22:27:24 +0530

    Fixes blocking-on-input issue for isomorphic strings solution
    Signed-off-by: Abhishek Kashyap <avskksyp@gmail.com>

--- isomorphic_strings/solution.py
+++ isomorphic_strings/solution.py
@@ -46,30 +46,30 @@ class Solution:
 # Driver Code Starts
 # Initial Template for Python 3
 
-import atexit
-import io
-import sys
-from collections import defaultdict
+# import atexit
+# import io
+# import sys
+# from collections import defaultdict
 
-_INPUT_LINES = sys.stdin.read().splitlines()
-input = iter(_INPUT_LINES).__next__
-_OUTPUT_BUFFER = io.StringIO()
-sys.stdout = _OUTPUT_BUFFER
-
-
-@atexit.register
-def write():
-    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+# _INPUT_LINES = sys.stdin.read().splitlines()
+# input = iter(_INPUT_LINES).__next__
+# _OUTPUT_BUFFER = io.StringIO()
+# sys.stdout = _OUTPUT_BUFFER
 
 
-if __name__ == '__main__':
-    t = int(input())
-    for i in range(t):
-        s = str(input())
-        p = str(input())
-        ob = Solution()
-        if (ob.areIsomorphic(s, p)):
-            print(1)
-        else:
-            print(0)
+# @atexit.register
+# def write():
+#     sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+# if __name__ == '__main__':
+#     t = int(input())
+#     for i in range(t):
+#         s = str(input())
+#         p = str(input())
+#         ob = Solution()
+#         if (ob.areIsomorphic(s, p)):
+#             print(1)
+#         else:
+#             print(0)
 # } Driver Code Ends
